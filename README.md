b8bd878a9dc98e56cb6727467d90e1343233eb14
KunalSoni <Kunal94611@gmail.com>
2023-04-26 12:40:18 +0530

    New Start

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactEcommerce
-Ecommerce in React
--- src/redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-// ** Reducers Imports
-import layout from "./layout";
-import navbar from "./navbar";
-
-const rootReducer = { navbar, layout };
-
-export default rootReducer;
